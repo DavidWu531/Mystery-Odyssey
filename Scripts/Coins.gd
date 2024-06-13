0532a8b8a29cace2ac777dc7ef9624744dcb9b6e
Nosweat420 <davidwu93447@gmail.com>
2024-06-14 11:44:18 +1200

    Experimenting with NPC dialogues

--- Scripts/Coins.gd
+++ Scripts/Coins.gd
@@ -1,24 +1,9 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	set_deferred("monitorable", true)
 	set_deferred("monitoring", true)
 	$CollisionShape2D.set_deferred("disabled", false)
-	
-	SignalBus.player_died.connect(player_died)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
-
-func player_died():
-	if $"../../Checkpoint/AnimatedSprite2D".animation == "Default":
-		show()
-		set_deferred("monitorable", true)
-		set_deferred("monitoring", true)
-		$CollisionShape2D.set_deferred("disabled", false)
 
 func _on_body_entered(body):
 	if "Player" in body.name:
---
+++ Scripts/GrassManager.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+var npci_dialogue = 0
+
+func _process(_delta):
+	if Input.is_action_just_pressed("Interact"):
+		if $NPCs/NPCI/Interactable.visible:
+			if npci_dialogue == 0:
+				$NPCs/NPCI/Dialogue.text = "Hello, traveller"
+			elif npci_dialogue == 1:
+				$NPCs/NPCI/Dialogue.text = "Wait, where have I seen you before?"
+			elif npci_dialogue == 2:
+				$NPCs/NPCI/Dialogue.text = "Anyway, whatever you're doing, watch out for obstacles"
+			else:
+				$NPCs/NPCI/Dialogue.text = ""
+				$NPCs/NPCI/Interactable.hide()
+			npci_dialogue += 1
+	
+	
+
+func _on_npci_body_entered(body):
+	if "Player" in body.name:
+		$NPCs/NPCI/Interactable.show()
+		
+func _on_npci_body_exited(body):
+	if "Player" in body.name:
+		$NPCs/NPCI/Interactable.hide()
+		npci_dialogue = 0
+		$NPCs/NPCI/Dialogue.text = ""
