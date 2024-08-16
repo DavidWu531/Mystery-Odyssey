44d79cec251fcdff635b76e16fd1c3d192856882
Nosweat420 <davidwu93447@gmail.com>
2024-08-16 12:56:03 +1200

    Upgraded to 4.3

--- Scripts/DesertManager.gd
+++ Scripts/DesertManager.gd
@@ -8,7 +8,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	if Input.is_action_just_pressed("Interact"):
+		if $PyramidAccess/Interactable.visible:
+			print("Teleport Player")
+			
+	for node in $BoulderWreck.get_overlapping_bodies():
+		if "Boulder" in node.name:
+			node.queue_free()
 
 
 func checkpoint_iii_hit():
@@ -38,7 +44,24 @@ func _on_boulder_timer_timeout():
 	add_child(new_boulder)
 
 
-func _on_boulder_wreck_body_entered(body):
-	if "Boulder" in body.name:
-		await get_tree().create_timer(0.75).timeout
-		body.queue_free()
+func _on_hidden_key_body_entered(body: Node2D) -> void:
+	if "Player" in body.name:
+		hide()
+		set_deferred("monitorable", false)
+		set_deferred("monitoring", false)
+		$CollisionShape2D.set_deferred("disabled", true)
+		Global.score += 1
+
+
+func _on_pyramid_access_body_entered(body: Node2D) -> void:
+	if "Player" in body.name:
+		if not $Other/HiddenKey.visible:
+			$PyramidAccess/Interactable.show()
+		else:
+			$PyramidAccess/Error.show()
+
+
+func _on_pyramid_access_body_exited(body: Node2D) -> void:
+	if "Player" in body.name:
+		$PyramidAccess/Interactable.hide()
+		$PyramidAccess/Error.hide()
